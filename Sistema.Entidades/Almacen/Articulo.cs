b91d532f0b68e65e593b44f4fb210fd7426f06d7
Glendy <Glendy@DESKTOP-H5L0PJC>
2021-09-27 22:30:13 -0600

    Agregar archivos de proyecto.

---
+++ Sistema.Entidades/Almacen/Articulo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sistema.Entidades.Almacen
+{
+    class Articulo
+    {
+       public int Codigo { get; set; }
+
+       public string Nombre { get; set; }
+
+        public int Precio_venta { get; set; }
+
+        public int Stock { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public bool condicion { get; set; }
+    }
+}
---
+++ Sistema.Web/Controllers/CategoriasController.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sistema.Datos;
+using Sistema.Entidades.Almacen;
+using Sistema.Web.Models.Almacen;
+using Sistema.Web.Models.Almacen.Categoria;
+
+namespace Sistema.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly DbContextSistema _context;
+
+        public CategoriasController(DbContextSistema context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Categorias/Listar
+        [HttpGet("[action]")]
+        public async Task <IEnumerable<CategoriaViewModels>> Listar()
+        {
+          var categoria = await _context.Categorias.ToListAsync();
+            return categoria.Select(c => new CategoriaViewModels
+            {
+                idcategoria = c.idcategoria,
+                nombre = c.nombre,
+                descripcion = c.descripcion,
+                condicion = c.condicion
+            });
+        }
+
+        // GET: api/Categorias/Mostrar/1
+        [HttpGet("[action]/{id}")]
+        public async Task<ActionResult> Mostrar([FromRoute] int id)
+        {
+            var categoria = await _context.Categorias.FindAsync(id);
+
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(new CategoriaViewModels
+            {
+                idcategoria = categoria.idcategoria,
+                nombre = categoria.nombre,
+                descripcion = categoria.descripcion,
+                condicion = categoria.condicion
+            });
+        }
+
+        // PUT: api/Categorias/Actualizar
+        [HttpPut("[action]")]
+        public async Task<IActionResult> Actualizar([FromBody] ActualizarViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (model.idcategoria <= 0)
+            {
+                return BadRequest();
+            }
+
+            var categoria = await _context.Categorias.FirstOrDefaultAsync(
+                c => c.idcategoria == model.idcategoria);
+
+            if(categoria == null)
+            {
+                return NotFound();
+            }
+
+            categoria.nombre = model.nombre;
+            categoria.descripcion = model.descripcion;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                //Guardar Excepcion
+                return BadRequest();
+            }
+
+            return Ok();
+        }
+
+        // POST: api/Categorias/Crear
+        [HttpPost("[Action]")]
+        public async Task<ActionResult> Crear([FromBody] CrearViewModel model)
+        {
+            //este If activa las validaciones de DataAnnotations
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Categoria categoria = new Categoria
+            {
+                nombre = model.nombre,
+                descripcion = model.descripcion,
+                condicion = true
+            };
+
+            //Se guarda los cambio es una insercion
+            _context.Categorias.Add(categoria);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+            
+            return Ok();
+        }
+
+        // DELETE: api/Categorias/Eliminar
+        [HttpDelete("[action]/{id}")]
+        public async Task<ActionResult<Categoria>> Eliminar(int id)
+        {
+            var categoria = await _context.Categorias.FindAsync(id);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categorias.Remove(categoria);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+
+
+            return Ok();
+        }
+
+        // PUT: api/Categorias/Desactivar/1
+        [HttpPut("[action]/{id}")]
+        public async Task<IActionResult> Desactivar([FromRoute] int id)
+        {
+            
+            if (id <= 0)
+            {
+                return BadRequest();
+            }
+
+            var categoria = await _context.Categorias.FirstOrDefaultAsync(
+                c => c.idcategoria == id);
+
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            categoria.condicion = false;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                //Guardar Excepcion
+                return BadRequest();
+            }
+
+            return Ok();
+        }
+
+        // PUT: api/Categorias/Activar/1
+        [HttpPut("[action]/{id}")]
+        public async Task<IActionResult> Activar([FromRoute] int id)
+        {
+
+            if (id <= 0)
+            {
+                return BadRequest();
+            }
+
+            var categoria = await _context.Categorias.FirstOrDefaultAsync(
+                c => c.idcategoria == id);
+
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            categoria.condicion = true;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                //Guardar Excepcion
+                return BadRequest();
+            }
+
+            return Ok();
+        }
+
+        private bool CategoriaExists(int id)
+        {
+            return _context.Categorias.Any(e => e.idcategoria == id);
+        }
+    }
+}
